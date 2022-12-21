07a7b53e37e2511b8981e2af25a1cb13c26c0ad5
Paulo Perna <paulo.perna.rocha@gmail.com>
2022-12-21 16:42:54 -0300

    Resolucao Ultimo Exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Principal{
+    public class Program{
+        public static int funcaoRetornaQuantitadePOV(decimal porcentagem, int totalNegociado){
+            decimal resultado = Math.Floor(porcentagem*totalNegociado/(1-porcentagem));
+            return ((int)resultado);
+        }
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine($"Teste: {funcaoRetornaQuantitadePOV(0.1M, 90)}");
+            System.Console.WriteLine($"Teste: {funcaoRetornaQuantitadePOV(0.1M, 100)}");
+            System.Console.WriteLine($"Teste: {funcaoRetornaQuantitadePOV(0.2M, 70)}");
+        }
+    }
+}
